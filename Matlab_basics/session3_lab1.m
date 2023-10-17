54a8bb0512c524f77be86c61a114c745f7e0d925
HossamMuhaammed <146008256+HossamMuhaammed@users.noreply.github.com>
2023-10-17 19:57:01 +0300

    Add files via upload

---
+++ Matlab_basics/session3_lab1.m
@@ -0,0 +1,8 @@
+
+function sumResult = mySimpleFunction(num1, num2)
+    sumResult = num1 + num2;
+end
+
+% Call mySimpleFunction with sample inputs and display the result
+result= mySimpleFunction(5, 7);
+disp(['Task 1: Sum Result = ' num2str(result1)]);
