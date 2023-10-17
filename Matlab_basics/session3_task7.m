e14ec142aac5e486130abffd0bd4e314c61b72d1
HossamMuhaammed <146008256+HossamMuhaammed@users.noreply.github.com>
2023-10-17 20:32:44 +0300

    Add files via upload

---
+++ Matlab_basics/session3_task7.m
@@ -0,0 +1,9 @@
+vector = [3, 7, 1, 9, 4, 8];
+
+maxValue = max(vector);
+minValue = min(vector);
+sumValue = sum(vector);
+
+disp(['Task 7: Max Value = ' num2str(maxValue)]);
+disp(['Task 7: Min Value = ' num2str(minValue)]);
+disp(['Task 7: Sum Value = ' num2str(sumValue)]);
