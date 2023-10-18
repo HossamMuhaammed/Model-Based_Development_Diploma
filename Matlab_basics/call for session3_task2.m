ee654ed79235266496abdc021c609ecac54de5b5
HossamMuhaammed <146008256+HossamMuhaammed@users.noreply.github.com>
2023-10-18 20:15:36 +0300

    Add files via upload

---
+++ Matlab_basics/call for session3_task2.m
@@ -0,0 +1,8 @@
+% Call calculateCircleArea with different radii and display the results
+radius1 = 3;
+radius2 = 6;
+area1 = calculateCircleArea(radius1);
+area2 = calculateCircleArea(radius2);
+disp('Area 1 = ',num2str(area1));
+disp('Area 2 = ',num2str(area2));
+clc
