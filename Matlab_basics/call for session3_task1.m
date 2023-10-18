1460bb882331f7cdc83651d45b94532b8feb96ce
HossamMuhaammed <146008256+HossamMuhaammed@users.noreply.github.com>
2023-10-18 20:04:53 +0300

    Add files via upload

---
+++ Matlab_basics/call for session3_task1.m
@@ -0,0 +1,2 @@
+result1 = mySimpleFunction(5, 7);
+disp(['Task 1: Sum Result = ' num2str(result1)]);
---
+++ Matlab_basics/session3_task1.m
@@ -0,0 +1,3 @@
+function sumResult = mySimpleFunction(num1, num2)
+    sumResult = num1 + num2;
+end
