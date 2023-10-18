519b6a478f2bea432e2deaa7dfbd9ef7f4d9772f
HossamMuhaammed <146008256+HossamMuhaammed@users.noreply.github.com>
2023-10-18 20:31:01 +0300

    Update session3_task5.m

--- Matlab_basics/session3_task5.m
+++ Matlab_basics/session3_task5.m
@@ -2,13 +2,3 @@ function result = applyFunction(func, vector)
     result = func(vector);
 end
 
-% Test applyFunction with various function handles
-squareFunc = @(x) x.^2;
-cubeFunc = @(x) x.^3;
-
-data = [1, 2, 3];
-squareResult = applyFunction(squareFunc, data);
-cubeResult = applyFunction(cubeFunc, data);
-
-disp(['Task 5: Square Function Result = ' num2str(squareResult)]);
-disp(['Task 5: Cube Function Result = ' num2str(cubeResult)]);
